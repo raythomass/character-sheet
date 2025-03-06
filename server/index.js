a335f8637b875ec8f7487b4a6537d97999566375
Ray Thomas <raythomas@Rays-MacBook-Air.local>
2025-03-06 15:21:09 -0700

    connected database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("Server Setup")
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next()
+})
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Database Connected, Listening on Port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+    
