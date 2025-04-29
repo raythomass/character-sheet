d44acf2887eee80da8aae84e971b50d4d72b89ba
raythomass <rthomas5678@gmail.com>
2025-04-29 12:47:25 -0700

    signup posts to db

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
+import { SheetContextProvider } from './context/SheetContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthContextProvider>
+      <SheetContextProvider>
+        <App />
+    </SheetContextProvider>
+    </AuthContextProvider>
   </StrictMode>,
 )
