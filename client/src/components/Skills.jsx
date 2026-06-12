6b5debba0458839db0d0987dc89dc7794ecd285d
raythomass <rthomas5678@gmail.com>
2026-06-12 16:20:43 -0700

    skills component can now change proficiency

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,111 +1,84 @@
 import React from 'react';
 import { skillList } from '../configs/skillsList';
+import { useUpdateCharacter } from '../hooks/useUpdateCharacter';
 
-export default function Skills({skills, stats, proficiencyBonus}) {
+export default function Skills({characterId, skills, stats, proficiencyBonus}) {
+    const { updateCharacter } = useUpdateCharacter();
 
     const getModifier = (score) => Math.floor((score - 10) / 2);
     
+    const plusOrMinus = (mod) => {
+        if (mod >= 0) {
+            return (
+                `+${mod}`
+            )
+        } else {
+            return (
+                `${mod}`
+            )
+        }
+    }
+
+    const getProfMultiplier = (level) => {
+        if (level === 0) return 0;     // none
+        if (level === 1) return 0.5;   // half proficiency
+        if (level === 2) return 1;     // proficiency
+        if (level === 3) return 2;     // expertise
+        return 0;
+    };
+
+    const getSkillBonus = (skill) => {
+        const abilityMod = getModifier(stats[skill.ability]);
+
+        const level = skills?.[skill.key]?.proficiency_level || 0;
+
+        return (
+            abilityMod + proficiencyBonus * getProfMultiplier(level)
+        );
+    };
+
+    const handleSkillToggle = async (skillKey) => {
+        const currentLevel = skills?.[skillKey]?.proficiency_level || 0;
+
+        const newLevel =
+            currentLevel === 0 ? 1 :
+            currentLevel === 1 ? 2 :
+            currentLevel === 2 ? 3:
+            0;
+        
+        await updateCharacter(characterId, {
+            [`skills.${skillKey}.proficiency_level`] :
+            newLevel,
+        });
+    }
 
   return (
-    <div className='flex flex-wrap border-2 border-amber-950'>
-        {skillList.map((skill) => (
-        <div key={skill.key} className='skill-container flex gap-4 items-center p-2'>
-            <div className='skill-prof'></div>
-            <small>{skill.label}</small>
-        </div>
-    ))}
+    <div className='flex flex-wrap justify-between'>
+        {skillList.map((skill) => {
+            const level = skills?.[skill.key]?.proficiency_level || 0
+
+            return (
+                <div key={skill.key} className='skill-container flex gap-4 justify-between items-center p-2'>
+                    <div
+                        className={`skill-prof cursor-pointer ${
+                            level === 0 ? "none" :
+                            level === 1 ? "half-prof" :
+                            level === 2 ? "prof" :
+                            "expertise"
+                        }`}
+                        onClick={() =>
+                            handleSkillToggle(skill.key)
+                         }
+                    />
+                    <small>{skill.label}</small>
+                    <h6>
+                        {getSkillBonus(skill) >= 0
+                            ? `+${getSkillBonus(skill)}`
+                            : getSkillBonus(skill)}
+                    </h6>
+                </div>
+            )
+    })}
     </div>
-    // <div className='skills flex flex-wrap justify-between'>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Acrobatics</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.dexterity)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Animal Handling</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.wisdom)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Arcana</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.intelligence)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Athletics</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.strength)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Deception</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.charisma)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>History</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.intelligence)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Insight</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.wisdom)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Intimidation</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.charisma)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Investigation</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.intelligence)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Medicine</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.wisdom)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Nature</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.intelligence)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Perception</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.wisdom)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Perfomance</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.charisma)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Persuasion</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.charisma)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Religion</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.intelligence)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Sleight of Hand</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.dexterity)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Stealth</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.dexterity)}</h6>
-    //     </div>
-    //     <div className='skill-container flex gap-4 items-center justify-between p-2'>
-    //         <div className='skill-prof'></div>
-    //         <h7>Survival</h7>
-    //         <h6 className='font-bold'>+{getModifier(stats.wisdom)}</h6>
-    //     </div>
-    // </div>
   );
 }
